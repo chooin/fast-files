de0cf0baee7fd5696e562b048519869dd6783a29
Devin.Zhu <chooin@126.com>
2021-05-13 13:21:21 +0800

    chore: utils type

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -2,14 +2,14 @@ const getType = (v: any): string => {
   return Object.prototype.toString.call(v).slice(8, -1)
 }
 
-export const isObject = (v: any) => {
+export const isObject = (v: any): boolean => {
   return getType(v) === 'Object'
 }
 
-export const isUndefined = (v: any) => {
+export const isUndefined = (v: any): boolean => {
   return getType(v) === 'Undefined'
 }
 
-export const isDefined = (v: any) => {
+export const isDefined = (v: any): boolean => {
   return !isUndefined(v)
 }
